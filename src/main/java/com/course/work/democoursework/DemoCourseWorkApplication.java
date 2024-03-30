58e00fd6b3ea7b8e92fdee9da7dd5ea4fdf79772
datigrov <denistigrov@icloud.com>
2024-03-30 16:45:01 +0300

    first step

---
+++ src/main/java/com/course/work/democoursework/DemoCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package com.course.work.democoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCourseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCourseWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/work/democoursework/DemoCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.work.democoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCourseWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
