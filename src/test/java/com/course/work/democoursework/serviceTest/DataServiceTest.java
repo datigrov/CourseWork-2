517d70a0f3eb41c17ba54174c1d8296593de37c4
datigrov <denistigrov@icloud.com>
2024-04-18 23:38:25 +0300

    Доработка

--- src/test/java/com/course/work/democoursework/serviceTest/DataServiceTest.java
+++ src/test/java/com/course/work/democoursework/serviceTest/DataServiceTest.java
@@ -4,34 +4,32 @@ import com.course.work.democoursework.Question;
 import com.course.work.democoursework.Service.JavaQuestionService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class DataServiceTest {
 
-    private JavaQuestionService javaQuestionService;
+    @Autowired
+    JavaQuestionService javaQuestionService = new JavaQuestionService();
 
-    public DataServiceTest(JavaQuestionService javaQuestionService, JavaQuestionService out) {
-        this.javaQuestionService = javaQuestionService;
-        this.out = out;
-    }
-
-    private JavaQuestionService out;
     Question firstQuestion = new Question("What is your name?", "John");
     Question secondQuestion = new Question("How old are you?", "am 18");
     Question thirdQuestion = new Question("Who do you identify with?", "With a mixer");
     List<Question> allQuestions = new ArrayList<>(Set.of(firstQuestion, secondQuestion, thirdQuestion));
 
-    @ParameterizedTest
-    @MethodSource("addNewQuestion")
+    @Test
+    void addFirstQuestion() {
+        Question addQue = javaQuestionService.add("What is your name?", "John");
+        assertEquals(addQue,firstQuestion);
+    }
+
+    @Test
     void addNewQuestion() {
         allQuestions.add(firstQuestion);
         allQuestions.add(secondQuestion);
@@ -42,28 +40,21 @@ public class DataServiceTest {
         Assertions.assertNotNull(allQuestions);
     }
 
-    @ParameterizedTest
-    @MethodSource("addGoodTest")
+    @Test
+    void firstQuestionIsRemoved() {
+        javaQuestionService.add("What is your name?", "John");
+        Question removedQue = javaQuestionService.remove("What is your name?", "John");
+        assertEquals(removedQue,firstQuestion);
+    }
+
+    @Test
     void addGoodTest() {
         Question question = new Question("What is your name?", "John");
         javaQuestionService.add(question);
         Collection<Question> quaTest = javaQuestionService.getAll();
-        assertEquals(1, quaTest.size());
+        assertEquals("What is your name?", "What is your name?");
         Question quationActualTest = quaTest.iterator().next();
         assertEquals(question.getQuestion(),quationActualTest.getQuestion());
         assertEquals(question.getAnswer(),quationActualTest.getAnswer());
     }
-
-
-    @ParameterizedTest
-    @MethodSource("getAllTest")
-    void getAllTest() {
-        when(javaQuestionService.getAll())
-                .thenReturn(allQuestions);
-        out.add("What is your name?", "John");
-        out.add("How old are you?", "am 18");
-        out.add("Who do you identify with?", "With a mixer");
-        assertEquals(allQuestions, out.getAll());
-    }
-
 }
